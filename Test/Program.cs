fb5a4b1942d042be43763c06e03a878a8a4c8ead
Poppon66 <81710852+Poppon66@users.noreply.github.com>
2021-04-12 20:02:05 -0400

    V2
    Poppon Ajout Consoleread

--- Test/Program.cs
+++ Test/Program.cs
@@ -8,6 +8,8 @@ namespace Test
         {
             int a = 0;
 
+            Console.Write("Tapez la valeur de a: ");
+            a = int.Parse(Console.ReadLine());
             Console.WriteLine("a= "+a);
         }
     }
